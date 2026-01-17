b173e91525ddf83d0d7915906bd3616025e13052
Chidi Orji <orjichidi95@gmail.com>
2026-01-17 02:22:08 +0000

    initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import WorkOSProvider from '../integrations/workos/provider'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <WorkOSProvider>
+        <Header />
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </WorkOSProvider>
+    </>
+  ),
+})
